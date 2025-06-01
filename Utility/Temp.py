85f5cd159a00182e0e654c52529547f32f7c9a2e
Matthias Gugler <guglermatthias@gmail.com>
2025-06-01 22:48:23 +0200

    refactor: improve folder structure. TestImage missing due to LFS-quota

--- Utility/Temp.py
+++
@@ -1,8 +0,0 @@
-import threading
-
-from PipelineModules.FrameCapturer import FrameCapturer
-
-stop_event = threading.Event
-frameCapturer = FrameCapturer(stop_event)
-print(frameCapturer.measure_camera_fps(1000))
-
